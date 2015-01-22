d2aa7c7519d29d35be5f6dc1727dd1d0c6f9d210
amoad-hashimoto <hashimoto@amoad.com>
2015-01-22 20:24:41 +0900

    Native_initListViewSample
    リストビューのサンプルアプリを追加しました。

---
+++ Samples/AMoAdNativeListView/AMoAdNativeListView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMoAdNativeListView
+//
+//  Created by AMoAd on 2015/01/22.
+//  Copyright (c) 2015年 AMoAd, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
