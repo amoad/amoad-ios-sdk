b043c8eb445d787bd7b7651e6e89c96c792e3c63
amoad-hashimoto <hashimoto@amoad.com>
2015-02-24 17:12:25 +0900

    NativeSample_addNativeAppIconTextLink
    リンクViewを指定するサンプルコード（ネイティブApp、アイコン＋テキスト）を追加しました。

---
+++ Samples/AMoAdNativeAppIconTextLink/AMoAdNativeAppIconTextLink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMoAdNativeAppIconTextLink
+//
+//  Created by AMoAd on 2015/02/24.
+//  Copyright (c) 2015年 AMoAd, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
