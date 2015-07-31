07ee0e9b5cc0888b0e4d31bc2ab35ec0eb70a6d0
amoad-hashimoto <hashimoto@amoad.com>
2015-07-31 19:27:10 +0900

    NativePreRoll_addSampleApp
    プリロール広告のサンプルアプリを追加しました。

---
+++ Samples/NativePreRoll/AMoAdNativePreRoll/AMoAdNativePreRoll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMoAdNativePreRoll
+//
+//  Created by AMoAd on 2015/07/31.
+//  Copyright (c) 2015年 AMoAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
