813624769a4e7740bf9f654a6aa26fb5d367db4d
amoad-hashimoto <hashimoto@amoad.com>
2015-08-05 19:36:18 +0900

    DocumentsInstall_add_AMoAdSample_update
    新しいインストールガイドを追加しました。
    ディスプレイ広告のサンプルを更新しました。

--- Samples/Display/AMoAdSample/AMoAdSample/ViewController.m
+++ Samples/Display/AMoAdSample/AMoAdSample/ViewController.m
@@ -2,41 +2,26 @@
 //  ViewController.m
 //  AMoAdSample
 //
-//  Created by AMoAdGeek on 2014/09/12.
-//  Copyright (c) 2014年 AMoAd, Inc. All rights reserved.
+//  Created by AMoAd on 2015/08/05.
+//  Copyright (c) 2015年 AMoAd. All rights reserved.
 //
 
 #import "ViewController.h"
-#import "AMoAdView.h"
 
-@interface ViewController () <AMoAdViewDelegate>
-@property (weak, nonatomic) IBOutlet AMoAdView *amoadView;
+@interface ViewController ()
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-
-    self.amoadView.delegate = self;
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)AMoAdViewDidReceiveAd:(AMoAdView *)amoadView {
-    NSLog(@"正常に広告を受信した");
-}
-
-- (void)AMoAdViewDidFailToReceiveAd:(AMoAdView *)amoadView error:(NSError *)error {
-    NSLog(@"広告の取得に失敗した");
-}
-
-- (void)AMoAdViewDidReceiveEmptyAd:(AMoAdView *)amoadView {
-    NSLog(@"空の広告を受信した");
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 @end
--- Samples/Display/AMoAdSample/AMoAdSample/main.m
+++ Samples/Display/AMoAdSample/AMoAdSample/main.m
@@ -2,15 +2,15 @@
 //  main.m
 //  AMoAdSample
 //
-//  Created by AMoAdGeek on 2014/09/12.
-//  Copyright (c) 2014年 AMoAd, Inc. All rights reserved.
+//  Created by AMoAd on 2015/08/05.
+//  Copyright (c) 2015年 AMoAd. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
 }
