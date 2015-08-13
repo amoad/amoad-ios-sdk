c681214e85a17464e54735b55f8fed84e7cb95aa
amoad-hashimoto <hashimoto@amoad.com>
2015-08-13 19:53:42 +0900

    Interstitial_addSdkSample
    インタースティシャル広告をSDKから直接、表示するサンプルコードを追加しました。

---
+++ Samples/DisplayInterstitial/AMoAdInterstitialAdSample/AMoAdInterstitialAdSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMoAdInterstitialAdSample
+//
+//  Created by AMoAd on 2015/08/13.
+//  Copyright (c) 2015年 AMoAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
