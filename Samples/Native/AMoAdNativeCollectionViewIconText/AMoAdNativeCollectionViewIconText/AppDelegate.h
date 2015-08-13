cec22e821af2a6c518ee3feeb3b028f606c5db32
島田 武史 <shimada_takeshi_gn@cyberagent.co.jp>
2015-08-13 14:33:18 +0900

    CollectionView_addDocuments

---
+++ Samples/Native/AMoAdNativeCollectionViewIconText/AMoAdNativeCollectionViewIconText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMoAdNativeCollectionViewIconText
+//
+//  Created by AMoAd on 2015/04/22.
+//  Copyright (c) 2015年 AMoAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Samples/Native/AMoAdNativeCollectionViewText/AMoAdNativeCollectionViewText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMoAdNativeCollectionViewText
+//
+//  Created by AMoAd on 2015/04/22.
+//  Copyright (c) 2015年 AMoAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
