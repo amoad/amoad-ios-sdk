f21c3ae7ec097a9dee41261f86d9ca53c0d78dad
amoad-hashimoto <hashimoto@amoad.com>
2015-12-09 17:39:35 +0900

    SampleWebView_init
    WebViewサポート機能のサンプルを追加しました。

---
+++ Samples/WebView/AMoAdWebViewSample/AMoAdWebViewSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMoAdWebViewSample
+//
+//  Created by AMoAd on 2015/12/09.
+//  Copyright © 2015年 AMoAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Samples/WebView/AMoAdWebViewSample/AMoAdWebViewSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AMoAdWebViewSample
+//
+//  Created by AMoAd on 2015/12/09.
+//  Copyright © 2015年 AMoAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
