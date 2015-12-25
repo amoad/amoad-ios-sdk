8a49972269ff6af9f28d675bb240ac5f272ee51d
yunkwanho1 <yunkwanho@gmail.com>
2015-12-25 11:25:01 +0900

    UnityPluginSampleFix

--- Samples/UnityPlugin/AMoAdUnityPluginInterstitialSample/Assets/Scripts/AMoAdUnityPluginInterstitialSample.cs
+++ Samples/UnityPlugin/AMoAdUnityPluginInterstitialSample/Assets/Scripts/AMoAdUnityPluginInterstitialSample.cs
@@ -6,12 +6,12 @@ public class AMoAdUnityPluginInterstitialSample : MonoBehaviour {
 	private static string SID = "管理画面から発行されるsidを設定してください";
 	private static string PANEL_IMAGE_NAME = "パネルに使用する画像ファイル名";
 
-	static AMoAdUnityPluginInterstitialSample () {
+	void Awake () {
+		//広告準備
 		AMoAdUnityPlugin.RegisterInterstitialAd (sid:SID);
 		AMoAdUnityPlugin.SetInterstitialPanel (sid:SID, imageName:PANEL_IMAGE_NAME);
-	}
-
-	void Awake () {
+		
+		//広告表示
 		AMoAdUnityPlugin.ShowInterstitialAd (SID);
 	}
 }
