9e7e24206534fb376401aa95bbd87cfc3b38bf37
amoad-hashimoto <hashimoto@amoad.com>
2016-01-29 17:24:33 +0900

    NativeHTML
    [WIP] ネイティブHTML広告のサンプル

---
+++ Samples/NativeApp/AMoAdNativeApp/AMoAdNativeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMoAdNativeApp
+//
+//  Created by AMoAd on 2016/01/29.
+//  Copyright © 2016年 AMoAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Samples/NativeApp/AMoAdNativeApp/AMoAdNativeApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AMoAdNativeApp
+//
+//  Created by AMoAd on 2016/01/29.
+//  Copyright © 2016年 AMoAd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
