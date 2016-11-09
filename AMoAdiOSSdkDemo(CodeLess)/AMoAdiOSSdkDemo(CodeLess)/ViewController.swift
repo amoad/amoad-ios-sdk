6e5e53068309087c60010d36ddd9ca2d6d48c856
yun <yun@amoad.com>
2016-11-09 15:34:41 +0900

    DemoAppCodeLess

---
+++ AMoAdiOSSdkDemo(CodeLess)/AMoAdiOSSdkDemo(CodeLess)/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AMoAdiOSSdkDemo(CodeLess)
+//
+//  Created by AMoAd on 2016/08/29.
+//  Copyright © 2016年 AMoAd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO Main.storyboard > ImageView > Attributeのsidを設定してください
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
